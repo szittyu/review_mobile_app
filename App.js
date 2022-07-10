ecccdce302051d7d486c3033b86841a9fd636cb0
szittyu <szittyu1@gmail.com>
2022-07-10 16:08:37 +0200

    navigate and routing logic made

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screen/home';
+import About from './screen/about';
+import ReviewDetails from './screen/reviewDetails';
 
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
 export default function App() {
-  // const [fontsLoaded, setFontsLoaded] = useState(false);
 
   const [fontsLoaded] = useFonts({
     'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
@@ -19,7 +25,13 @@ export default function App() {
     )
   } else {
     return (
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="About" component={About} />
+          <Stack.Screen name="ReviewDetails" component={ReviewDetails} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
--- screen/home.js
+++ screen/home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function Home() {
