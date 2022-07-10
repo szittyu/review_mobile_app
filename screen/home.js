5bd85ddbbf0d4161c432cec390483325f110b5e5
szittyu <szittyu1@gmail.com>
2022-07-10 16:39:24 +0200

    buttons added home and review page

--- screen/home.js
+++ screen/home.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home() {
+export default function Home({ navigation }) {
+
+    const pressHandler = () => {
+        navigation.push('ReviewDetails')
+    }
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button
+                title='Go to Review Details'
+                onPress={pressHandler}
+            />
         </View>
     );
 }
--- screen/reviewDetails.js
+++ screen/reviewDetails.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+
+    const pressHandler = () => {
+        navigation.goBack();
+    }
+
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>ReviewDetails screen</Text>
+            <Text style={globalStyles.titleText}>Review Details screen</Text>
+            <Button
+                title="back to home screen"
+                onPress={pressHandler}
+            />
         </View>
     )
 }
